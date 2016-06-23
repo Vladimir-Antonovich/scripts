6d55413adde16b4be0a04dacfd0ae4d8b598ebc5
Vladimir Antonovich <vladimir_antonovich@gigaspaces.com>
2016-06-23 12:25:31 +0300

    Pre stop Ravello application script
    # This script avoid few issues with Cloudify Manager when Ravello
    application stops
    
    | Celery Management              | dead| - this is an issue after
    restart Ravello Application

---
+++ Ravello-scripts/pre-save-blueprint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Author: Vladimir Antonovich
+# This script avoid few issues with Cloudify Manager when Ravello application stops
+# This script should be run from cloudify cli folder before Ravello application will stop. 
+#
+
+cd ~/cfy
+CLOUDIFY=$(awk 'match($0, /_management_ip:/){print $3}' .cloudify/context | tr -d \')
+USER=$(awk 'match($0, /_management_user:/){print $3}' .cloudify/context | tr -d \')
+KEY=$(awk 'match($0, /_management_key:/){print $3}' .cloudify/context | tr -d \'~)
+
+ssh_command1="sudo systemctl stop cloudify-mgmtworker.service;sudo systemctl stop cloudify-influxdb.service"
+
+ssh -i $HOME$KEY $USER@$CLOUDIFY $ssh_command
